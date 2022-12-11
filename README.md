36799ce97aea541f24df34575a28908c833b7c7b
Kaehler <nils.kaehler@gmail.com>
2022-12-12 00:10:36 +0100

    Adding chat and test.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AiFriend
 A small Ai friend developed with openAI
+
+# API key for openAI
+Add a file named `openaiapikey` to the root and copy your api key from openai into it.
+
--- chat.py
+++ chat.py
@@ -10,7 +10,7 @@ openai.api_key = open_file('openaiapikey')
 print(open_file('openaiapikey'))
 
 
-def gpt_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['USER:', 'NILS:']):
+def gpt_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['USER:', 'JAX:']):
     gpt_prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
     gpt_response = openai.Completion.create(
         engine=engine,
@@ -26,8 +26,13 @@ def gpt_completion(prompt, engine='text-davinci-003', temp=0.7, top_p=1.0, token
 
 
 if __name__ == '__main__':
-    print(openai.api_key)
-    main_prompt = 'Give me an instruction of how to implement openai api in python'
-    response = gpt_completion(main_prompt)
-    print(response)
+    conversation = list()
+    while True:
+        user_input = input('USER: ')
+        conversation.append('USER: %s' % user_input)
+        text_block = '\n'.join(conversation)
+        main_prompt = open_file('assets/chatprompt').replace('<<BLOCK>>', text_block)
+        main_prompt = main_prompt + '\nJAX:'
+        response = gpt_completion(main_prompt)
+        print('JAX:', response)
 
