e4302fb12e064a8ae1d350e18e507e2d9bcf18a2
Kaehler <nils.kaehler@gmail.com>
2022-12-14 09:33:20 +0100

    Adding data and parsing classifiers

---
+++ classification.py
@@ -0,0 +1,42 @@
+# import json
+#
+# # Open the JSON file
+# with open("data.json", "r") as f:
+#     # Parse the JSON
+#     data = json.load(f)
+#
+# # Access the values in the parsed JSON
+# print(data["name"])  # Output: John
+# print(data["age"])  # Output: 30
+# print(data["city"])  # Output: New York
+import re
+import numpy as np
+
+words = ['b', 'a', 'a', 'c', 'c', 'c']
+
+
+with open("data/question_response", "r") as f:
+    # Read each line of the file
+    types = []
+    for line in f:
+        # Print the line (without the newline character at the end)
+        # The regular expression pattern to match
+        pattern = r"^\d+\.\s+"
+        result = re.sub(pattern, "", line)
+
+        # Replace the matched pattern with the empty string
+        if not result.startswith("#") and not len(result.strip()) == 0:
+
+            types.append(result.strip())
+            # print(result.strip())
+
+    values, counts = np.unique(types, return_counts=True)
+    # print(values, counts)
+
+    values_and_counts = zip(values, counts)
+
+    # Iterate over the list of tuples and print each one
+    for value, count in values_and_counts:
+        # print(count, '\t', value)
+        print("'{t}' |".format(t=value))
+
---
+++ todo.md
@@ -0,0 +1,5 @@
+# TODO
+
+- Add key to path instead. (like in documentation)
+- Add chat messages, and fix conversation
+- Add a classifier problem solver.
